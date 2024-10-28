498bdeb9532554194ce2e747bee59cb86d6414af
MasZam12 <135273176+MasZam12@users.noreply.github.com>
2024-10-29 05:01:32 +0700

    perubahan

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-from db import create_connection
+from database.db import create_connection
 import mysql.connector
 
 app = Flask(__name__)
